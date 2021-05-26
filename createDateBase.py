f7340110a249fce3a13d9c2998dca5bb33222071
fatmaosama429 <61249791+fatmaosama429@users.noreply.github.com>
2021-05-26 23:44:29 +0200

    Add files via upload

--- createDateBase.py
+++ createDateBase.py
@@ -29,29 +29,31 @@ for filename in os.listdir():
     if filename.endswith(".mp3"):
         
         mp3_audio = AudioSegment.from_file((filename), format="mp3")[:60000]  # read mp3
-        wname = mktemp('.wav')  # use temporary file
-        mp3_audio.export(wname, format="wav")  # convert to wav
-        wavsong,samplingFrequency =librosa.load(wname)
+        wavsong = np.array(mp3_audio.get_array_of_samples()).astype(np.float16)
+        samplingFrequency = mp3_audio.frame_rate
 
+        print(wavsong)
 
-        # sampleFreqs,sampleTime, colorMesh =signal.spectrogram(wavsong[:][0],fs=samplingFrequency)
+        sampleFreqs,sampleTime, colorMesh =signal.spectrogram(wavsong,fs=samplingFrequency)
+
         feature1= librosa.feature.mfcc(y=wavsong, sr=samplingFrequency)
-        feature2= librosa.feature.melspectrogram(y=wavsong, sr=samplingFrequency)
-        feature3= librosa.feature.chroma_stft(y=wavsong, sr=samplingFrequency)
+        feature2= librosa.feature.melspectrogram(y=wavsong, sr=samplingFrequency,S=colorMesh)
+        feature3= librosa.feature.chroma_stft(y=wavsong, sr=samplingFrequency,S=colorMesh)
+        # lib.feature.melspectrogram(y=data,sr=rate,S=color)
 
-        # spect_image=Image.fromarray(colorMesh)
+        spect_image=Image.fromarray(colorMesh)
         new_image = Image.fromarray(feature1)
         new_image2=Image.fromarray(feature2)
         new_image3=Image.fromarray(feature3)
 
-        # spectHash=imagehash.phash(spect_image)
-        firstHash=imagehash.phash(new_image)
-        secondHash=imagehash.phash(new_image2)
-        thirdHash=imagehash.phash(new_image3)
+        spectHash=imagehash.phash(spect_image, hash_size=16).__str__()
+        firstHash=imagehash.phash(new_image, hash_size=16).__str__()
+        secondHash=imagehash.phash(new_image2, hash_size=16).__str__()
+        thirdHash=imagehash.phash(new_image3, hash_size=16).__str__()
 
         Song.append(filename)
 
-        # spect_hash_list.append(spectHash)
+        spect_hash_list.append(spectHash)
         mfccHashList.append(firstHash)
         melSpectroHashList.append(secondHash)
         chromaHashList.append(thirdHash)
@@ -59,8 +61,10 @@ for filename in os.listdir():
 
 
 
-# dict = {'song': Song, 'spectrogram hash':spect_hash_list,'mfcc hash': mfccHashList,'mel spectrogram hash':melSpectroHashList,'Chroma_stft hash':chromaHashList}
-dict = {'song': Song,'mfcc hash': mfccHashList,'mel spectrogram hash':melSpectroHashList,'Chroma_stft hash':chromaHashList}
+dict = {'song': Song, 'spectrogram hash':spect_hash_list,\
+    'mfcc hash': mfccHashList,'mel spectrogram hash':melSpectroHashList,\
+        'Chroma_stft hash':chromaHashList}
+# dict = {'song': Song,'mfcc hash': mfccHashList,'mel spectrogram hash':melSpectroHashList,'Chroma_stft hash':chromaHashList}
 
 df = pd.DataFrame(dict)
 print(df.head)
